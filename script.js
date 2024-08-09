a659e688d4c787db9dde06d5820ae20aca2e625e
jirkuc <jir.kuc@outlook.cz>
2024-08-09 13:14:50 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,147 @@
+'use strict';
+
+let totalPrice = 0;
+let ticketPrice = 0;
+let ticketBudget = 0;
+
+// let ticketClassFilledIn = true;
+let ticketClassPriceIncrease = 1.0;
+
+let ticketBudgetFilledIn = false;
+
+const asDestination = document.getElementById('as-destination');
+const asPieces = document.getElementById('as-pcs');
+const asWayBackChb = document.getElementById('as-way-back-checkbox');
+let WayBackBackChbValue = 1;
+
+const asPriceCalculationBtn = document.getElementById(
+  'as-price-calculation-btn'
+);
+const asPriceCalculationResult = document.getElementById(
+  'as-price-calculation-result'
+);
+
+const ticketClass = document.querySelector('.as-class');
+
+const asTicketBudgetValue = document.getElementById('as-ticket-budget');
+const asTicketBudgetValidationResult = document.getElementById(
+  'as-price-validation-result'
+);
+const asTicketBudgetBtn = document.getElementById('as-ticket-budget-btn');
+
+const asNote = document.getElementById('as-note');
+const asNoteValidationResult = document.getElementById(
+  'as-note-validation-result'
+);
+
+const calculateTotalPrice = () => {
+  let asMessage = '';
+  //if (ticketClassFilledIn) {
+  totalPrice =
+    parseInt(asDestination.value) *
+    parseInt(asPieces.value) *
+    ticketClassPriceIncrease;
+  if (asWayBackChb.checked) {
+    totalPrice *= 2;
+  }
+  asMessage = `Výsledná částka: ${totalPrice} Kč`;
+  //} else {
+  //  asMessage = 'Vyberte třídu letenek.';
+  //}
+  asPriceCalculationResult.textContent = asMessage;
+};
+
+calculateTotalPrice();
+
+const calculateTicketBudget = () => {
+  ticketBudget = parseInt(asTicketBudgetValue.value);
+  let asBudgetMessage = '';
+
+  let asBudgetMessageClass = '';
+
+  let asPriceDifference = totalPrice - ticketBudget;
+  if (asPriceDifference > 0) {
+    asBudgetMessage = `Rozpočet na letenku není dostatečný, chybí ${Math.abs(
+      asPriceDifference
+    )} Kč.`;
+    asBudgetMessageClass = 'text-danger fw-bolder px-1';
+  } else if (asPriceDifference < 0) {
+    asBudgetMessage = `Rozpočet na letenku je dostatečný, zbude ${Math.abs(
+      asPriceDifference
+    )} Kč.`;
+    asBudgetMessageClass = 'text-success fw-medium px-1';
+  } else {
+    asBudgetMessage = 'Máte to tak akorát, nezbude nic.';
+    asBudgetMessageClass = 'text-warning fw-semibold px-1';
+  }
+  asTicketBudgetValidationResult.textContent = asBudgetMessage;
+  asTicketBudgetValidationResult.setAttribute('class', asBudgetMessageClass);
+};
+
+const verifyTicketBudget = () => {
+  if (asTicketBudgetValue.value && totalPrice > 0) {
+    ticketBudgetFilledIn = true;
+    asTicketBudgetBtn.removeAttribute('disabled');
+  } else {
+    ticketBudgetFilledIn = false;
+    asTicketBudgetBtn.setAttribute('disabled', true);
+  }
+  if (asTicketBudgetValue.value === '') {
+    asTicketBudgetValidationResult.textContent = '';
+  }
+};
+
+// asPriceCalculationBtn.addEventListener('click', function () {
+//   calculateTotalPrice();
+//   verifyTicketBudget();
+// });
+
+asDestination.addEventListener('change', function () {
+  calculateTotalPrice();
+  verifyTicketBudget();
+});
+
+asPieces.addEventListener('change', function () {
+  calculateTotalPrice();
+  verifyTicketBudget();
+});
+
+asWayBackChb.addEventListener('change', function () {
+  calculateTotalPrice();
+  verifyTicketBudget();
+});
+
+ticketClass.addEventListener('change', function (event) {
+  // ticketClassFilledIn = true;
+  ticketClassPriceIncrease = parseFloat(event.target.value);
+  // asPriceCalculationBtn.removeAttribute('disabled');
+  calculateTotalPrice();
+  verifyTicketBudget();
+});
+
+asTicketBudgetValue.addEventListener('input', function () {
+  verifyTicketBudget();
+});
+
+asTicketBudgetBtn.addEventListener('click', function () {
+  calculateTicketBudget();
+});
+
+asNote.addEventListener('input', function () {
+  const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/g;
+  let validationMessage = '';
+  let validationMessageClass = '';
+  let validationFieldValue = asNote.value;
+
+  if (!asNote.value) {
+    asNoteValidationResult.textContent = '';
+  } else if (specialCharPattern.test(validationFieldValue)) {
+    validationMessage = 'Pole není v pořádku';
+    validationMessageClass = 'text-danger fw-bolder px-1';
+  } else {
+    validationMessage = 'Pole je v pořádku';
+    validationMessageClass = 'text-success fw-medium px-1';
+  }
+  asNoteValidationResult.textContent = validationMessage;
+  asNoteValidationResult.setAttribute('class', validationMessageClass);
+});
